b1603f0ba31ce6312a1b2bd87dba6d737b080ce8
gaetanccg <gaetan6942@gmail.com>
2025-11-24 12:25:21 +0100

    fix from rebase

--- frontend/src/views/Chat.vue
+++ frontend/src/views/Chat.vue
@@ -1,37 +1,23 @@
 <template>
-  <v-app>
-    <v-app-bar color="green-darken-1" dark>
-      <v-app-bar-title>WhatsApp Clone</v-app-bar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon variant="text" @click="openSessions">
-        <v-icon>mdi-history</v-icon>
-      </v-btn>
-      <v-btn icon @click="goProfile" title="Mon profil">
-        <v-avatar size="36">
-          <img v-if="authStore.user?.avatar" :src="authStore.user.avatar" alt="avatar" />
-          <span v-else style="font-weight:bold;color:white">{{ authStore.user?.username?.[0] || '?' }}</span>
-        </v-avatar>
-      </v-btn>
-      <v-chip class="mr-2" color="green-lighten-1">
-        {{ authStore.user?.username }}
-      </v-chip>
-      <v-btn icon @click="handleLogout">
-        <v-icon>mdi-logout</v-icon>
-      </v-btn>
-    </v-app-bar>
     <v-app>
         <v-app-bar color="green-darken-1" dark>
             <v-app-bar-title>WhatsApp Clone</v-app-bar-title>
             <v-spacer></v-spacer>
+            <v-btn icon variant="text" @click="openSessions">
+                <v-icon>mdi-history</v-icon>
+            </v-btn>
+
             <v-btn icon @click="goProfile" title="Mon profil">
                 <v-avatar size="36">
                     <img v-if="authStore.user?.avatar" :src="authStore.user.avatar" alt="avatar" />
                     <span v-else style="font-weight:bold;color:white">{{ authStore.user?.username?.[0] || '?' }}</span>
                 </v-avatar>
             </v-btn>
+
             <v-chip class="mr-2" color="green-lighten-1">
                 {{ authStore.user?.username }}
             </v-chip>
+
             <v-btn icon @click="handleLogout">
                 <v-icon>mdi-logout</v-icon>
             </v-btn>
@@ -58,31 +44,23 @@
             </v-container>
         </v-main>
 
-    <v-dialog v-model="showSessions" max-width="800">
-      <v-card>
-        <v-card-title class="d-flex align-center">
-          <v-icon class="mr-2">mdi-shield-account</v-icon> Sessions & Historique
-          <v-spacer></v-spacer>
-          <v-btn icon variant="text" @click="showSessions=false">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-        </v-card-title>
-        <v-divider></v-divider>
-        <v-card-text>
-          <SessionManager />
-        </v-card-text>
-      </v-card>
-    </v-dialog>
+        <v-dialog v-model="showSessions" max-width="800">
+            <v-card>
+                <v-card-title class="d-flex align-center">
+                    <v-icon class="mr-2">mdi-shield-account</v-icon>
+                    Sessions & Historique
+                    <v-spacer></v-spacer>
+                    <v-btn icon variant="text" @click="showSessions = false">
+                        <v-icon>mdi-close</v-icon>
+                    </v-btn>
+                </v-card-title>
+                <v-divider></v-divider>
+                <v-card-text>
+                    <SessionManager />
+                </v-card-text>
+            </v-card>
+        </v-dialog>
 
-    <v-snackbar
-      v-model="showNotification"
-      :timeout="3000"
-      color="green"
-      location="top right"
-    >
-      {{ notificationMessage }}
-    </v-snackbar>
-  </v-app>
         <v-snackbar
             v-model="showNotification"
             :timeout="3000"
@@ -165,12 +143,9 @@ onUnmounted(() => {
 });
 
 const openSessions = () => {
-  showSessions.value = true;
+    showSessions.value = true;
 };
 
-const handleLogout = async () => {
-  await authStore.backendLogout();
-  router.push('/login');
 const handleLogout = () => {
     authStore.logout();
     router.push('/login');
