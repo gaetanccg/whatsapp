2c022633e970ce2cd5a83f13fa60c10f81d7abf2
gaetanccg <116770253+gaetanccg@users.noreply.github.com>
2025-11-25 10:13:47 +0100

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -83,8 +83,8 @@ app.use(cors(corsOptions));
 // Ensure preflight requests are handled
 app.options('*', cors(corsOptions));
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
